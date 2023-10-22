3d61455b642a49fe8b8661d6791ed9c6a4229834
Sinchiguano <cesarsinchiguano@hotmail.es>
2023-10-22 11:19:16 -0500

    my first hello world in python

---
+++ helloWorld.py
@@ -0,0 +1,21 @@
+print("Sinchiguano César")
+import time
+
+myNumber_1=123
+mynumber_1=456
+
+
+counter=100
+
+
+
+
+while(counter>10):
+    print(counter)
+    counter=counter-1
+    time.sleep(1)
+
+c=100
+
+print('The number saved is the following: {}'.format(myNumber_1))
+print('The number saved is the following: {}'.format(mynumber_1))
